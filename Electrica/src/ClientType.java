2844f4a83a0e177c7a6c2f226a6111f6d3d64829
CarlosDeMoraGil <carlosdemoragil825@gmail.com>
2023-11-25 20:10:56 +0100

    Completado el main

---
+++ Electrica/src/ClientType.java
@@ -0,0 +1,56 @@
+public class ClientType {
+    private String code;
+    private String address;
+    private String population;
+    private String province;
+    private String mail;
+    private String phoneNumber;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(String population) {
+        this.population = population;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
--- Electrica/src/Main.java
+++ Electrica/src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String args[]){
-        Freelance f1 = new Freelance();
+        Freelance a1 = new Freelance();
         Companies c1 = new Companies();
 
         Products p1 = new Products();
@@ -11,88 +11,91 @@ public class Main {
         Services s1 = new Services();
         Services s2 = new Services();
 
+        Bill f1 = new Bill();
+
         Scanner sc = new Scanner(System.in);
 
         //DATOS AUTONOMOS
         System.out.println("INTRODUCE LOS DATOS DEL AUTONOMO: ");
 
-        f1.setCode("456");
+        a1.setCode("456");
 
         System.out.println("Introduce el nombre:  ");
         String name = sc.nextLine();
-        f1.setName(name);
+        a1.setName(name);
 
         System.out.println("Introduce el apellido:  ");
         String apellidos = sc.nextLine();
-        f1.setSurname(apellidos);
+        a1.setSurname(apellidos);
 
         System.out.println("Introduce el dni: ");
         String dni = sc.nextLine();
-        f1.setId(dni);
+        a1.setId(dni);
 
         System.out.println("Introduce la direccion:  ");
         String direc = sc.nextLine();
-        f1.setAddress(direc);
+        a1.setAddress(direc);
 
         System.out.println("Introduce la poblacion:  ");
         String pobla = sc.nextLine();
-        f1.setPopulation(pobla);
+        a1.setPopulation(pobla);
 
         System.out.println("Introduce la provincia:  ");
         String provin = sc.nextLine();
-        f1.setProvince(provin);
+        a1.setProvince(provin);
 
         System.out.println("Introduce el email:  ");
         String email = sc.nextLine();
-        f1.setMail(email);
+        a1.setMail(email);
 
         System.out.println("Introduce el telefono:  ");
         String telfn = sc.nextLine();
-        f1.setPhoneNumber(telfn);
+        a1.setPhoneNumber(telfn);
 
         System.out.println("  ");
-        System.out.println("Codigo: " + f1.getCode());
-        System.out.println("Nombre: " + f1.getName());
-        System.out.println("Segundo Nombre: " + f1.getSurname());
-        System.out.println("DNI: " + f1.getId());
-        System.out.println("Telefono: " + f1.getPhoneNumber());
-        System.out.println("Email: " + f1.getMail());
-        System.out.println("Provincia: " + f1.getProvince());
-        System.out.println("Poblacion: " + f1.getPopulation());
-        System.out.println("Direccion: " + f1.getAddress());
+        System.out.println("Codigo: " + a1.getCode());
+        System.out.println("Nombre: " + a1.getName());
+        System.out.println("Segundo Nombre: " + a1.getSurname());
+        System.out.println("DNI: " + a1.getId());
+        System.out.println("Telefono: " + a1.getPhoneNumber());
+        System.out.println("Email: " + a1.getMail());
+        System.out.println("Provincia: " + a1.getProvince());
+        System.out.println("Poblacion: " + a1.getPopulation());
+        System.out.println("Direccion: " + a1.getAddress());
 
         //DATOS SOCIEDAD
+        System.out.println("  ");
         System.out.println("INTRODUCE LOS DATOS DE LA SOCIEDAD: ");
 
-        f1.setCode("548");
+        a1.setCode("548");
 
         System.out.println("Introduce el cif: ");
         String cif = sc.nextLine();
-        f1.setId(cif);
+        a1.setId(cif);
 
         System.out.println("Introduce la razon: ");
         String motive = sc.nextLine();
-        f1.setId(motive);
+        a1.setId(motive);
 
         System.out.println("Introduce la direccion:  ");
         String direcc = sc.nextLine();
-        f1.setAddress(direcc);
+        a1.setAddress(direcc);
 
         System.out.println("Introduce la poblacion:  ");
         String poblac = sc.nextLine();
-        f1.setPopulation(poblac);
+        a1.setPopulation(poblac);
 
         System.out.println("Introduce la provincia:  ");
         String provinc = sc.nextLine();
-        f1.setProvince(provinc);
+        a1.setProvince(provinc);
 
         System.out.println("Introduce el email:  ");
         String mail = sc.nextLine();
-        f1.setMail(mail);
+        a1.setMail(mail);
 
         System.out.println("Introduce el telefono:  ");
         String telfno = sc.nextLine();
-        f1.setPhoneNumber(telfno);
+        a1.setPhoneNumber(telfno);
 
         System.out.println("  ");
         System.out.println("Codigo: " + c1.getCode());
@@ -104,6 +107,7 @@ public class Main {
         System.out.println("Direccion: " + c1.getAddress());
 
         //DATOS PRODUCTO 1
+        System.out.println("  ");
         System.out.println("INTRODUCE LOS DATOS DEL PRIMER PRODUCTO: ");
 
         p1.setCode("878");
@@ -137,7 +141,8 @@ public class Main {
         System.out.println("Tipo de IVA: " + p1.getIva());
 
         //DATOS PRODUCTO 2
-        System.out.println("INTRODUCE LOS DATOS DEL PRIMER PRODUCTO: ");
+        System.out.println("  ");
+        System.out.println("INTRODUCE LOS DATOS DEL SEGUNDO PRODUCTO: ");
 
         p2.setCode("878");
 
@@ -170,6 +175,7 @@ public class Main {
         System.out.println("Tipo de IVA: " + p2.getIva());
 
         //DATOS SERVICIO 1
+        System.out.println("  ");
         System.out.println("INTRODUCE LOS DATOS DEL PRIMER SERVICIO: ");
 
         s1.setCode("878");
@@ -193,6 +199,7 @@ public class Main {
         System.out.println("Tipo de IVA: " + s1.getIva());
 
         //DATOS SERVICIO 2
+        System.out.println("  ");
         System.out.println("INTRODUCE LOS DATOS DEL SEGUNDO SERVICIO: ");
 
         s2.setCode("878");
@@ -214,6 +221,35 @@ public class Main {
         System.out.println("Nombre: " + s2.getName());
         System.out.println("Precio: " + s2.getPrize());
         System.out.println("Tipo de IVA: " + s2.getIva());
+        
+        //FACTURA
+        System.out.println(" ");
+        System.out.println("DATOS DE LA FACTURA");
+
+        a1.setCode("61460");
+
+        System.out.println("Introduce una fecha de la factura: ");
+        String f1dt = sc.nextLine();
+        f1.setDate(f1dt);
+
+        System.out.println("  ");
+        System.out.println("FACTURA");
+        System.out.println("Codigo: " + f1.getCode());
+        System.out.println("Fecha: " + f1.getDate());
+        System.out.println("DATOS CLIENTE");
+        System.out.println("CIF: " + c1.getCif());
+        System.out.println("Email: " + c1.getMail());
+        System.out.println("Provincia: " + c1.getProvince());
+        System.out.println("Poblacion: " + c1.getPopulation());
+        System.out.println("Direccion: " + c1.getAddress());
+        System.out.println("DATOS VENTA");
+        System.out.println("Codigo: " + p1.getCode());
+        System.out.println("Nombre: " + p1.getName());
+        System.out.println("Precio: " + p1.getPrize());
+        System.out.println("Tipo de IVA: " + p1.getIva());
+        System.out.println("TOTAL" + p1.getPrize() + p2.getPrize());
+
+
     }
 }
 
